61a8526810460264596441e9eef6744fad0bc1bb
Antanas <antanas.zapasnikas@gmail.com>
2021-06-30 16:33:46 +0300

    initial commit

---
+++ src/main/java/lt/zap/fxrates/FxratesApplication.java
@@ -0,0 +1,13 @@
+package lt.zap.fxrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxratesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/zap/fxrates/FxratesApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.zap.fxrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
